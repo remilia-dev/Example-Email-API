c99016b117ddaf2a526d061c5ef67ad0369753ef
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-21 01:40:22 -0600

    Add model classes
    The plan is for emails to be logged in a database using Entity Framework Core.
    
    The design of the model is relatively simple to allow for quick insertions
    while still keeping track of all the necessary information. While one could
    de-duplicate addresses by using a separate table (with a unique column),
    this is not done as it would slow down the throughput.

---
+++ BaseAPI/Model/Message.cs
@@ -0,0 +1,53 @@
+﻿using MimeKit;
+
+namespace BaseAPI.Model
+{
+    public class Message
+    {
+        public int Id { get; private set; }
+        public string Sender { get; set; } = "";
+        public string Subject { get; set; } = "";
+        public string HtmlBody { get; set; } = "";
+        public bool? WasSent { get; set; }
+        public DateTime CreatedOn { get; internal set; }
+        public DateTime LastModifiedOn { get; internal set; }
+
+        public virtual ICollection<Recipient> Recipients { get; set; } = new List<Recipient>();
+
+        /// <summary>
+        /// Converts this message into a MimeMessage.
+        /// </summary>
+        /// <exception cref="ParseException">
+        /// Thrown if <see cref="Sender"/> is an invalid email address or if
+        /// <see cref="Recipients">any recipient</see> has an invalid email address.
+        /// </exception>
+        public MimeMessage ToMimeMessage()
+        {
+            var bodyBuilder = new BodyBuilder()
+            {
+                HtmlBody = this.HtmlBody,
+            };
+
+            var mimeMessage = new MimeMessage()
+            {
+                Subject = this.Subject,
+                Body = bodyBuilder.ToMessageBody(),
+                Sender = MailboxAddress.Parse(Sender),
+            };
+
+            foreach (var recipient in Recipients)
+            {
+                var addressList = recipient.Type switch
+                {
+                    RecipientType.To => mimeMessage.To,
+                    RecipientType.Cc => mimeMessage.Cc,
+                    RecipientType.Bcc => mimeMessage.Bcc,
+                    _ => throw new NotImplementedException($"Unknown recipient type: {recipient.Type}"),
+                };
+                addressList.Add(MailboxAddress.Parse(recipient.Address));
+            }
+
+            return mimeMessage;
+        }
+    }
+}
---
+++ BaseAPI/Model/Recipient.cs
@@ -0,0 +1,23 @@
+﻿namespace BaseAPI.Model
+{
+    public class Recipient
+    {
+        public int Id { get; private set; }
+        public Message? Message { get; private set; }
+        public string Address { get; set; }
+        public RecipientType Type { get; set; }
+
+        public Recipient(string address, RecipientType type = RecipientType.To)
+        {
+            Address = address;
+            Type = type;
+        }
+    }
+
+    public enum RecipientType
+    {
+        To = 0,
+        Cc = 1,
+        Bcc = 2,
+    }
+}
