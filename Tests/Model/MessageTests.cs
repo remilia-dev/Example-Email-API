64c392f31e8b810cd0b665ffea230fb7be9ae87d
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-21 03:37:52 -0600

    Add model tests
    These tests encompass the primary behavior of the model classes.

---
+++ Tests/Model/MessageTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+using BaseAPI.Model;
+using MimeKit;
+using Xunit;
+
+namespace Tests.Model
+{
+    public class MessageTests
+    {
+        [Fact]
+        public void ToMimeMessage_CorrectlyConvertsValidMessage()
+        {
+            const string TO_RECIPIENT = "to@example.org";
+            const string CC_RECIPIENT = "carbon_copy@example.org";
+            const string BCC_RECIPIENT = "blind_carbon_copy@example.org";
+            var validMessage = new Message()
+            {
+                Sender = "sender@example.org",
+                Subject = "Subject Line",
+                HtmlBody = "<b>HTML Body</b>",
+                Recipients = new List<Recipient>()
+                {
+                    new Recipient(TO_RECIPIENT),
+                    new Recipient(CC_RECIPIENT, RecipientType.Cc),
+                    new Recipient(BCC_RECIPIENT, RecipientType.Bcc),
+                },
+            };
+
+            var mimeMessage = validMessage.ToMimeMessage();
+
+            Assert.Equal(validMessage.Sender, mimeMessage.Sender.Address);
+            Assert.Equal(validMessage.Subject, mimeMessage.Subject);
+            Assert.Equal(validMessage.HtmlBody, mimeMessage.HtmlBody);
+            Assert.Equal(TO_RECIPIENT,
+                Assert.Single(mimeMessage.To).ToString());
+            Assert.Equal(CC_RECIPIENT,
+                Assert.Single(mimeMessage.Cc).ToString());
+            Assert.Equal(BCC_RECIPIENT,
+                Assert.Single(mimeMessage.Bcc).ToString());
+        }
+
+        [Fact]
+        public void ToMimeMessage_ThrowsOnInvalidSender()
+        {
+            var invalidMessage = new Message()
+            {
+                Sender = "not a valid email address",
+            };
+
+            Assert.Throws<ParseException>(()
+                => invalidMessage.ToMimeMessage());
+        }
+
+        [Fact]
+        public void ToMimeMessage_ThrowsOnInvalidRecipient()
+        {
+            var invalidMessage = new Message()
+            {
+                Sender = "valid@example.org",
+                Recipients = new List<Recipient>()
+                {
+                    new Recipient("definitely not a valid email")
+                },
+            };
+
+            Assert.Throws<ParseException>(()
+                => invalidMessage.ToMimeMessage());
+        }
+    }
+}
---
+++ Tests/Model/RecipientTests.cs
@@ -0,0 +1,25 @@
+﻿using BaseAPI.Model;
+using Xunit;
+
+namespace Tests.Model
+{
+    public class RecipientTests
+    {
+        [Fact]
+        public void Recipient_Type_DefaultsToTo()
+        {
+            Assert.Equal(RecipientType.To,
+                new Recipient("valid@example.org").Type);
+        }
+
+        [Fact]
+        public void RecipientType_IntValuesRemainTheSame()
+        {
+            // It's important to ensure that RecipientType enum values keep the same int value as
+            // changing them would also change how saved values are interpreted.
+            Assert.Equal(0, (int)RecipientType.To);
+            Assert.Equal(1, (int)RecipientType.Cc);
+            Assert.Equal(2, (int)RecipientType.Bcc);
+        }
+    }
+}
