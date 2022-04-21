6adadfa9e9e47381a67610ddc56c233f82590b4f
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-21 03:41:37 -0600

    Add MessageDbContext and associated tests
    MessageDbContext is a fairly standard DbContext with the exception that
    it manages Message.CreatedOn and Message.LastModifiedOn. By having
    the context manage it, the creation and modification times are
    automatic.

---
+++ BaseAPI/Data/MessageDbContext.cs
@@ -0,0 +1,54 @@
+﻿using BaseAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BaseAPI.Data
+{
+    public class MessageDbContext : DbContext
+    {
+        public DbSet<Message> Messages => Set<Message>();
+        public DbSet<Recipient> Recipients => Set<Recipient>();
+
+        public MessageDbContext(DbContextOptions options) : base(options) { }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            BeforeSaveChanges();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            BeforeSaveChanges();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        protected virtual void BeforeSaveChanges()
+        {
+            foreach (var entity in ChangeTracker.Entries<Message>())
+            {
+                var message = entity.Entity;
+                switch (entity.State)
+                {
+                    case EntityState.Added:
+                        message.CreatedOn = DateTime.UtcNow;
+                        message.LastModifiedOn = message.CreatedOn;
+                        break;
+                    case EntityState.Modified:
+                        message.LastModifiedOn = DateTime.UtcNow;
+                        break;
+                }
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>()
+                .Property(e => e.CreatedOn)
+                .Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
+            modelBuilder.Entity<Recipient>()
+                .Property(e => e.Type)
+                .HasConversion<int>();
+        }
+    }
+}
---
+++ Tests/Data/MessageDbContextTests.cs
@@ -0,0 +1,59 @@
+﻿using BaseAPI.Data;
+using BaseAPI.Model;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Data
+{
+    public class MessageDbContextTests
+    {
+        private readonly MessageDbContext _context;
+
+        public MessageDbContextTests()
+        {
+            var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder()
+                .UseSqlite(connection)
+                .Options;
+            _context = new MessageDbContext(options);
+            _context.Database.EnsureCreated();
+        }
+
+        [Fact]
+        public async Task NewMessages_HaveCorrectCreatedAndLastModifiedTime()
+        {
+            var message = new Message();
+            await _context.Messages.AddAsync(message);
+            var beforeSave = DateTime.UtcNow;
+            await _context.SaveChangesAsync();
+            var afterSave = DateTime.UtcNow;
+
+            Assert.InRange(message.CreatedOn, beforeSave, afterSave);
+            // CreationOn and LastModifiedOn should be exactly the same on new messages.
+            Assert.Equal(message.CreatedOn, message.LastModifiedOn);
+        }
+
+        [Fact]
+        public async Task UpdatedMessages_HaveOnlyLastModifiedOnUpdated()
+        {
+            var message = new Message();
+            await _context.Messages.AddAsync(message);
+            await _context.SaveChangesAsync();
+            var previousCreatedOn = message.CreatedOn;
+
+            message.Subject = "A Change Occured";
+            var beforeSave = DateTime.UtcNow;
+            await _context.SaveChangesAsync();
+            var afterSave = DateTime.UtcNow;
+
+            Assert.InRange(message.LastModifiedOn, beforeSave, afterSave);
+            Assert.NotEqual(message.CreatedOn, message.LastModifiedOn);
+            // The CreatedOn field did *not* change due to modifications
+            Assert.Equal(previousCreatedOn, message.CreatedOn);
+        }
+    }
+}
