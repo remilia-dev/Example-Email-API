f58598147970c8527428a35ffd62f268b17bdf60
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-25 01:30:46 -0600

    Add SmtpEmailTransport to Mailer.MailKit project
    SmtpEmailTransport has been designed to work well with ASP.NET's
    dependency injection.

---
+++ MailKit/SmtpConnectionOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Mailer.MailKit;
+
+public class SmtpConnectionOptions
+{
+    public string Host { get; set; } = "";
+    public int Port { get; set; } = 0;
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
---
+++ MailKit/SmtpEmailTransport.cs
@@ -0,0 +1,37 @@
+﻿using Mailer.Core.Model;
+using Mailer.Core.Services;
+using MailKit;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using System.Net.Sockets;
+
+namespace Mailer.MailKit;
+
+public class SmtpEmailTransport : IEmailTransport
+{
+    private readonly SmtpConnectionOptions _options;
+
+    public SmtpEmailTransport(IOptions<SmtpConnectionOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public async Task SendEmailAsync(Message message, CancellationToken cancelToken = default)
+    {
+        try
+        {
+            using var smtpClient = new SmtpClient();
+            await smtpClient.ConnectAsync(_options.Host, _options.Port, cancellationToken: cancelToken);
+            if (_options.Username != null)
+            {
+                await smtpClient.AuthenticateAsync(_options.Username, _options.Password, cancelToken);
+            }
+            await smtpClient.SendAsync(message.ToMimeMessage(), cancelToken);
+            await smtpClient.DisconnectAsync(true, cancelToken);
+        }
+        catch (Exception ex) when (ex is IOException or SocketException or ProtocolException or CommandException)
+        {
+            throw new TransportConnectionException("An exception occurred while sending an email.", ex);
+        }
+    }
+}
