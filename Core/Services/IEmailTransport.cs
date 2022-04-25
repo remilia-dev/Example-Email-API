49e8780b2432a4afba51812b155f1ac49dffb39d
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-25 01:16:27 -0600

    Add IEmailTransport interface
    The IEmailTransport interface is meant to abstract away the transporter
    of an email message. This will allow Mailer.Core to be used with a
    library other than MailKit if one wanted.
    
    Since the Mailer example project needs the ability to retry sending
    an email, a unified exception was needed for when retrying may
    work (it would be a waste to retry if the failure occurred due to
    missing credentials). TransportConnectionException exists for this
    reason.

---
+++ Core/Services/IEmailTransport.cs
@@ -0,0 +1,18 @@
+﻿using Mailer.Core.Model;
+
+namespace Mailer.Core.Services;
+
+public interface IEmailTransport
+{
+    /// <summary>
+    /// Asynchronously send an email message.
+    /// </summary>
+    /// <param name="message">The message to send. Must be a valid message.</param>
+    /// <param name="cancelToken">Token to cancel the operation.</param>
+    /// <exception cref="TransportConnectionException">
+    /// Thrown when the connection to send the email failed for non-authentication reasons.
+    /// Since this may be due to external variables (like Internet connectivity), trying
+    /// again may result in the email being sent.
+    /// </exception>
+    Task SendEmailAsync(Message message, CancellationToken cancelToken = default);
+}
---
+++ Core/Services/TransportConnectionException.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Mailer.Core.Services;
+
+[Serializable]
+public class TransportConnectionException : Exception
+{
+    public TransportConnectionException() { }
+
+    public TransportConnectionException(string? message)
+        : base(message) { }
+
+    public TransportConnectionException(string? message, Exception? innerException)
+        : base(message, innerException) { }
+
+    protected TransportConnectionException(SerializationInfo info, StreamingContext context)
+        : base(info, context) { }
+}
