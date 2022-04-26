97692c9ccc692859f73ed4253ca87a4b60c2e590
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-26 04:37:05 -0600

    Make CreatedOn and LastModifiedOn setters public
    Making them internal unfortunately prevents the OData API from
    displaying them.

--- Core/Model/EmailMessage.cs
+++ Core/Model/EmailMessage.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mailer.Core.Model;
 
@@ -7,12 +9,13 @@ namespace Mailer.Core.Model;
 public class EmailMessage
 {
     public int Id { get; set; }
+    [EmailAddress]
     public string Sender { get; set; } = "";
     public string Subject { get; set; } = "";
     public string HtmlBody { get; set; } = "";
     public bool? WasSent { get; set; }
-    public DateTime CreatedOn { get; internal set; }
-    public DateTime LastModifiedOn { get; internal set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime LastModifiedOn { get; set; }
 
     public virtual ICollection<EmailRecipient> Recipients { get; set; } = new List<EmailRecipient>();
 }
