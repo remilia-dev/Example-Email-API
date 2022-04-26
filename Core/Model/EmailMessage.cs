413cd575535c53f262f1128b106ab30293fb35a3
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-26 05:25:57 -0600

    Add NewEmailMessage to contain data in a new email
    When a user sends a new email to send, we need to only grab the
    properties that a user can provide. The best way to do this is to make
    a separate type that excludes any property we don't want a user
    setting.
    
    In this case, we don't want a user setting WasSent (CreatedOn and
    LastModifiedOn would be overwritten in EmailDbContext anyway).
    Thankfully, this case is simple enough that we can create a separate
    type and have EmailMessage inherit it.

--- Core/Model/EmailMessage.cs
+++ Core/Model/EmailMessage.cs
@@ -1,25 +1,13 @@
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace Mailer.Core.Model;
 
 [Index(nameof(Sender))]
 [Index(nameof(CreatedOn))]
-public class EmailMessage
+public class EmailMessage : NewEmailMessage
 {
     public int Id { get; set; }
-    [Required]
-    [EmailAddress]
-    public string? Sender { get; set; }
-    [Required]
-    public string? Subject { get; set; }
-    [Required]
-    public string? HtmlBody { get; set; }
     public bool? WasSent { get; set; }
     public DateTime CreatedOn { get; set; }
     public DateTime LastModifiedOn { get; set; }
-    [Required]
-    [MinLength(1)]
-    public virtual ICollection<EmailRecipient>? Recipients { get; set; }
 }
---
+++ Core/Model/NewEmailMessage.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mailer.Core.Model;
+public class NewEmailMessage
+{
+    [Required]
+    [EmailAddress]
+    public string? Sender { get; set; }
+    [Required]
+    public string? Subject { get; set; }
+    [Required]
+    public string? HtmlBody { get; set; }
+    [Required]
+    [MinLength(1)]
+    public virtual ICollection<EmailRecipient>? Recipients { get; set; }
+
+    public EmailMessage ToEmailMessage()
+    {
+        return new EmailMessage()
+        {
+            Sender = Sender,
+            Subject = Subject,
+            HtmlBody = HtmlBody,
+            Recipients = Recipients,
+        };
+    }
+}
