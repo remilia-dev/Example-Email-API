66e180a88842857616822374733f3e9378257f85
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-25 20:32:23 -0600

    Expand IEmailTransport interface
    IEmailTransport originally just had SendEmailAsync. This limits what is
    possible with the interface.
    
    I've figured out how I want to structure the email sending service.
    Because sending an email may take too long for an HTTP request,
    the sending part will be done by a background service that utilizes
    an IEmailTransport.

--- Core/Services/IEmailTransport.cs
+++ Core/Services/IEmailTransport.cs
@@ -4,15 +4,8 @@ namespace Mailer.Core.Services;
 
 public interface IEmailTransport
 {
-    /// <summary>
-    /// Asynchronously send an email message.
-    /// </summary>
-    /// <param name="message">The message to send. Must be a valid message.</param>
-    /// <param name="cancelToken">Token to cancel the operation.</param>
-    /// <exception cref="TransportConnectionException">
-    /// Thrown when the connection to send the email failed for non-authentication reasons.
-    /// Since this may be due to external variables (like Internet connectivity), trying
-    /// again may result in the email being sent.
-    /// </exception>
+    bool IsConnected { get; }
+    Task ConnectAsync(CancellationToken cancelToken = default);
+    Task DisconnectAsync(CancellationToken cancelToken = default);
     Task SendEmailAsync(EmailMessage message, CancellationToken cancelToken = default);
 }
--- MailKit/SmtpEmailTransport.cs
+++ MailKit/SmtpEmailTransport.cs
@@ -2,36 +2,82 @@
 using Mailer.Core.Services;
 using MailKit;
 using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.Extensions.Options;
+using MimeKit;
 using System.Net.Sockets;
 
 namespace Mailer.MailKit;
 
-public class SmtpEmailTransport : IEmailTransport
+public class SmtpEmailTransport : IEmailTransport, IDisposable
 {
     private readonly SmtpConnectionOptions _options;
+    private readonly SmtpClient _smtp;
 
     public SmtpEmailTransport(IOptions<SmtpConnectionOptions> options)
     {
         _options = options.Value;
+        _smtp = new SmtpClient();
     }
+
+    public void Dispose()
+    {
+        _smtp.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public bool IsConnected => _smtp.IsConnected;
+
+    public async Task ConnectAsync(CancellationToken cancelToken = default)
+    {
+        try
+        {
+            await _smtp.ConnectAsync(_options.Host, _options.Port, cancellationToken: cancelToken);
+            if (_options.Username != null)
+            {
+                await _smtp.AuthenticateAsync(_options.Username, _options.Password, cancelToken);
+            }
+        } catch (Exception ex) when (ex is SocketException or IOException or ProtocolException)
+        {
+            throw new TransportConnectionException("Failed to connect to SMTP server.", ex);
+        } catch (AuthenticationException ex)
+        {
+            throw new TransportConnectionException("Failed to authenticate with SMTP server.", ex);
+        }
+    }
+
+    public Task DisconnectAsync(CancellationToken cancelToken = default)
+        => _smtp.DisconnectAsync(true, cancelToken);
 
     public async Task SendEmailAsync(EmailMessage message, CancellationToken cancelToken = default)
     {
         try
         {
-            using var smtpClient = new SmtpClient();
-            await smtpClient.ConnectAsync(_options.Host, _options.Port, cancellationToken: cancelToken);
-            if (_options.Username != null)
-            {
-                await smtpClient.AuthenticateAsync(_options.Username, _options.Password, cancelToken);
-            }
-            await smtpClient.SendAsync(message.ToMimeMessage(), cancelToken);
-            await smtpClient.DisconnectAsync(true, cancelToken);
+            await _smtp.SendAsync(message.ToMimeMessage(), cancelToken);
         }
-        catch (Exception ex) when (ex is IOException or SocketException or ProtocolException or CommandException)
+        catch (ServiceNotConnectedException ex)
         {
-            throw new TransportConnectionException("An exception occurred while sending an email.", ex);
+            throw new TransportConnectionException(
+                "SMTP client was not connected when sending message. Connection may have been lost.",
+                ex);
+        }
+        catch (ServiceNotAuthenticatedException ex)
+        {
+            throw new TransportConnectionException(
+                "SMTP server requires authentication but none was provided.",
+                ex);
+        }
+        catch (Exception ex) when (ex is InvalidOperationException or ParseException)
+        {
+            throw new TransportConnectionException(
+                "Email message is invalid and cannot be sent.",
+                ex);
+        }
+        catch (Exception ex) when (ex is IOException or ProtocolException or CommandException)
+        {
+            throw new TransportConnectionException(
+                "An exception occurred while sending an email.",
+                ex);
         }
     }
 }
