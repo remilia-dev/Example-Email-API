9dd71b81b667b7275d8d0dc34d73d6f5ac8370bc
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-25 21:33:22 -0600

    Add IEmailQueue and EmailQueue
    To have a background service sending emails, there needs to be queue
    that controllers can access to add emails to be sent. IEmailQueue is
    the small interface that allows this, while EmailQueue is a simple
    implementation based on a Channel.

---
+++ Core/Services/EmailQueue.cs
@@ -0,0 +1,37 @@
+﻿using Mailer.Core.Model;
+using System.Threading.Channels;
+
+namespace Mailer.Core.Services;
+
+public class EmailQueue : IEmailQueue
+{
+    private readonly Channel<EmailMessage> _channel;
+    /// <summary>
+    /// Creates an unbounded email queue.
+    /// </summary>
+    public EmailQueue()
+    {
+        _channel = Channel.CreateUnbounded<EmailMessage>();
+    }
+    /// <summary>
+    /// Creates a bounded email queue. Callers adding to the queue will have to
+    /// wait if the queue reaches maximum capacity.
+    /// </summary>
+    /// <param name="bound">The maximum number of items the queue can hold.</param>
+    public EmailQueue(int bound)
+    {
+        _channel = Channel.CreateBounded<EmailMessage>(bound);
+    }
+
+    public Task EnqueueAsync(EmailMessage message, CancellationToken cancelToken = default)
+        => _channel.Writer.WriteAsync(message, cancelToken).AsTask();
+
+    public Task WaitForEmail(CancellationToken cancelToken = default)
+        => _channel.Reader.WaitToReadAsync(cancelToken).AsTask();
+
+    public EmailMessage? TryDequeue()
+    {
+        _channel.Reader.TryRead(out var message);
+        return message;
+    }
+}
---
+++ Core/Services/IEmailQueue.cs
@@ -0,0 +1,30 @@
+﻿using Mailer.Core.Model;
+
+namespace Mailer.Core.Services;
+
+public interface IEmailQueue
+{
+    /// <summary>
+    /// Adds an email message to the queue.
+    /// </summary>
+    /// <param name="message">The <see cref="EmailMessage"/> to add to the queue.</param>
+    /// <param name="cancelToken">A <see cref="CancellationToken"/> to cancel enqueue operation.</param>
+    /// <returns>
+    /// A <see cref="Task"/> that will complete when the email has been
+    /// added to the queue.
+    /// </returns>
+    Task EnqueueAsync(EmailMessage message, CancellationToken cancelToken = default);
+    /// <summary>
+    /// Waits until an email message has entered the queue.
+    /// </summary>
+    /// <param name="cancelToken">A <see cref="CancellationToken"/> to cancel the wait operation.</param>
+    /// <returns>
+    /// A <see cref="Task"/> that will complete when an email is in the queue.
+    /// </returns>
+    Task WaitForEmail(CancellationToken cancelToken = default);
+    /// <summary>
+    /// Attempts to remove an email message from the queue.
+    /// </summary>
+    /// <returns>The email message or null if there was none in the queue.</returns>
+    EmailMessage? TryDequeue();
+}
