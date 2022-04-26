015a3b1d008871bdd2541c1f3cd2cf9c16814538
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-26 06:13:57 -0600

    Add email posting to send an email
    Posting to `api/v1/Messages` will now enqueue the message if it is
    valid. It currently returns the created database entry to allow for
    checking the status later.

--- OData/Controllers/MessagesController.cs
+++ OData/Controllers/MessagesController.cs
@@ -1,5 +1,7 @@
 using Mailer.Core.Data;
 using Mailer.Core.Model;
+using Mailer.Core.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Formatter;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Results;
@@ -10,9 +12,12 @@ namespace Mailer.OData.Controllers;
 public class MessagesController : ODataController
 {
     private readonly EmailDbContext _db;
-    public MessagesController(EmailDbContext db)
+    private readonly IEmailQueue _emailQueue;
+
+    public MessagesController(EmailDbContext db, IEmailQueue emailQueue)
     {
         _db = db;
+        _emailQueue = emailQueue;
     }
 
     [EnableQuery]
@@ -21,4 +26,21 @@ public class MessagesController : ODataController
     [EnableQuery]
     public SingleResult<EmailMessage> GetEmailMessage([FromODataUri] int key)
         => SingleResult.Create(_db.Messages.Where(m => m.Id == key));
+
+    public async Task<IActionResult> Post([FromBody] NewEmailMessage newEmail)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var email = newEmail.ToEmailMessage();
+        await _db.Messages.AddAsync(email);
+        await _db.SaveChangesAsync();
+        _db.ChangeTracker.Clear();
+
+        await _emailQueue.EnqueueAsync(email);
+
+        return Created(email);
+    }
 }
