078077f50997ccff765d78640827e3aac634fdf3
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-26 05:27:17 -0600

    Move RecipientType to its own file
    While RecipientType is pretty simple, it's best to keep it in its own
    file.

--- Core/Model/EmailRecipient.cs
+++ Core/Model/EmailRecipient.cs
@@ -21,12 +21,3 @@ public class EmailRecipient
         Type = type;
     }
 }
-
-public enum RecipientType
-{
-    To = 0,
-    Cc = 1,
-    Bcc = 2,
-    Min = To,
-    Max = Bcc,
-}
---
+++ Core/Model/RecipientType.cs
@@ -0,0 +1,10 @@
+﻿namespace Mailer.Core.Model;
+
+public enum RecipientType
+{
+    To = 0,
+    Cc = 1,
+    Bcc = 2,
+    Min = To,
+    Max = Bcc,
+}
