0262e0b632b81108a354d0062e93460ffd24fde8
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-25 23:10:13 -0600

    Add BackgroundEmailService
    Background email service will watch an IEmailQueue and forward it to
    an IEmailTransport when there are emails to be sent.

---
+++ Core/Services/BackgroundEmailService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Mailer.Core.Services;
+
+public class BackgroundEmailService : BackgroundService
+{
+    private readonly IEmailQueue _emailQueue;
+    private readonly IServiceProvider _serviceProvider;
+
+    public BackgroundEmailService(IEmailQueue emailQueue, IServiceProvider serviceProvider)
+    {
+        _emailQueue = emailQueue;
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _emailQueue.WaitForEmail(stoppingToken);
+
+            using var scope = _serviceProvider.CreateAsyncScope();
+            var transport = scope.ServiceProvider.GetRequiredService<IEmailTransport>();
+            await transport.SendQueuedEmailsAsync(_emailQueue, stoppingToken);
+        }
+    }
+}
