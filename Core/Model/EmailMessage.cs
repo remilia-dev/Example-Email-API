e226488b80cd99d97c819c1ea14a0b41b46f3cb5
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-26 19:18:09 -0600

    Separate EmailMessage from NewEmailMessage
    It would be weird for EmailMessage to technically be a NewEmailMessage
    (since it inherits from it).

--- Core/Model/EmailMessage.cs
+++ Core/Model/EmailMessage.cs
@@ -5,19 +5,18 @@ namespace Mailer.Core.Model;
 
 [Index(nameof(Sender))]
 [Index(nameof(CreatedOn))]
-public class EmailMessage : NewEmailMessage
+public class EmailMessage
 {
     public int Id { get; set; }
+    [Required]
+    public string? Sender { get; set; }
+    [Required]
+    public string? Subject { get; set; }
+    [Required]
+    public string? HtmlBody { get; set; }
     public bool? WasSent { get; set; }
     public DateTime CreatedOn { get; set; }
     public DateTime LastModifiedOn { get; set; }
 
-    public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
-        // NewEmailMessage enforces that no Recipient has an Id, MessageId,
-        // or Message to ensure the data is "new".
-        //
-        // EmailMessage shouldn't have this constraint since it may be a
-        // message pulled from the database.
-        yield break;
-    }
+    public virtual ICollection<EmailRecipient>? Recipients { get; set; }
 }
