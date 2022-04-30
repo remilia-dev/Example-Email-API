7923647f8f29c412e0bba0bec0ce99b45f7f8c4f
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-30 00:13:55 -0600

    Add EmailQueueTests
    EmailQueue is also pretty simple, so there are not many tests. One
    could potentially test to ensure EmailQueue's behavior works in a
    multi-threaded scenario, but that would just be testing Channel's
    implementation at this point.

---
+++ Tests/Services/EmailQueueTests.cs
@@ -0,0 +1,51 @@
+﻿using Mailer.Core.Model;
+using Mailer.Core.Services;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Services;
+public class EmailQueueTests
+{
+    [Fact]
+    public async Task WaitForEmail_Throws_IfQueueEmptyAndOperationCancelled()
+    {
+        var cancelSource = new CancellationTokenSource();
+        cancelSource.Cancel();
+        var emailQueue = new EmailQueue();
+
+        await Assert.ThrowsAsync<TaskCanceledException>(()
+            => emailQueue.WaitForEmail(cancelSource.Token));
+    }
+
+    [Fact]
+    public void TryDequeue_ReturnsNull_IfNothingInQueue()
+    {
+        var emailQueue = new EmailQueue();
+
+        Assert.Null(emailQueue.TryDequeue());
+    }
+
+    [Fact]
+    public async Task TryDequeue_ReturnsFirstEmailInQueue()
+    {
+        var emailQueue = new EmailQueue();
+        var message = BlankEmailMessage();
+
+        await emailQueue.EnqueueAsync(message);
+
+        Assert.Same(message, emailQueue.TryDequeue());
+    }
+
+    private static EmailMessage BlankEmailMessage()
+    {
+        return new EmailMessage()
+        {
+            Sender = "",
+            Subject = "",
+            HtmlBody = "",
+            Recipients = new List<EmailRecipient>(),
+        };
+    }
+}
