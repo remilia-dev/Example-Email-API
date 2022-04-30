c5491f8f2a1ed7193a0589ff76abe57fc2410ddc
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-30 00:10:24 -0600

    Add NewEmailMessageTests
    There isn't much to test with NewEmailMessage since the class is simple.
    I did want to make sure two invariants hold:
    * That the result's Id is always 0 (since it's suppose to be a new
    record)
    * That the result's WasSent is always null (since we haven't attempted
    to send the email yet).

---
+++ Tests/Model/NewEmailMessageTests.cs
@@ -0,0 +1,29 @@
+﻿using Mailer.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Model;
+public class NewEmailMessageTests
+{
+    [Fact]
+    public void ToEmailMessage_Result_Id_Is0()
+    {
+        var message = new NewEmailMessage()
+            .ToEmailMessage();
+
+        Assert.Equal(0, message.Id);
+    }
+
+    [Fact]
+    public void ToEmailMessage_Result_WasSent_IsNull()
+    {
+        var message = new NewEmailMessage()
+            .ToEmailMessage();
+
+        Assert.Null(message.WasSent);
+    }
+}
