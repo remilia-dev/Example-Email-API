421e71e2e980290dea80ad9d6822cd8349be4aa8
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-29 19:55:08 -0600

    Document SmtpConnectionOptions

--- MailKit/SmtpConnectionOptions.cs
+++ MailKit/SmtpConnectionOptions.cs
@@ -1,16 +1,39 @@
 ﻿using Mailer.Core.Services;
+using MailKit.Security;
 using System.ComponentModel.DataAnnotations;
 
 namespace Mailer.MailKit;
 
 public class SmtpConnectionOptions : BaseEmailTransportOptions, IValidatableObject
 {
+    /// <summary>
+    /// The host SMTP server to connect to.
+    /// </summary>
     [Required]
     public string? Host { get; set; } = "";
+    /// <summary>
+    /// The port number to connect to. 0 will select the default port.
+    /// </summary>
+    /// <remarks>
+    /// The default port is 465 when <see cref="SecureSocketOptions"/> is
+    /// <see cref="SecureSocketOptions.SslOnConnect"/> and 25 otherwise.
+    /// </remarks>
     [Range(0, ushort.MaxValue)]
     public int Port { get; set; } = 0;
+    /// <summary>
+    /// An optional username to use to authenticate with the SMTP server. If not
+    /// <c>null</c>, then <see cref="Password"/> must not be <c>null</c> as well.
+    /// </summary>
     public string? Username { get; set; }
+    /// <summary>
+    /// An optional password to use to authenticate with the SMTP server. If not
+    /// <c>null</c>, then <see cref="Username"/> must not be <c>null</c> as well.
+    /// </summary>
     public string? Password { get; set; }
+    /// <summary>
+    /// Secure socket options when connecting to the SMTP server. See MailKit's
+    /// documentation for more information.
+    /// </summary>
     public SecureSocketOptions SecureSocketOptions { get; set; } = SecureSocketOptions.Auto;
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
