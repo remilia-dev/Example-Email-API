c7be98b8a8aca04ce27d7a190820916dc1787298
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-04-29 19:36:54 -0600

    Add validation to SmtpConnectionOptions

--- MailKit/SmtpConnectionOptions.cs
+++ MailKit/SmtpConnectionOptions.cs
@@ -1,11 +1,26 @@
 ﻿using Mailer.Core.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mailer.MailKit;
 
-public class SmtpConnectionOptions : BaseEmailTransportOptions
+public class SmtpConnectionOptions : BaseEmailTransportOptions, IValidatableObject
 {
-    public string Host { get; set; } = "";
+    [Required]
+    public string? Host { get; set; } = "";
+    [Range(0, ushort.MaxValue)]
     public int Port { get; set; } = 0;
     public string? Username { get; set; }
     public string? Password { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Username is not null && Password is null)
+        {
+            yield return new ValidationResult("Password property must be filled since Username is filled.");
+        } else if (Username is null && Password is not null)
+        {
+            yield return new ValidationResult("Username property must be filled since Password is filled.");
+        }
+        yield break;
+    }
 }
