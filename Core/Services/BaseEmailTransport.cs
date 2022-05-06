3c8463436353b8ba4e3ba6ee5ef0f3e96819dc4a
Remilia.Dev <62536855+remilia-dev@users.noreply.github.com>
2022-05-05 22:09:17 -0600

    Document BaseEmailTransport

--- Core/Services/BaseEmailTransport.cs
+++ Core/Services/BaseEmailTransport.cs
@@ -6,8 +6,18 @@ namespace Mailer.Core.Services;
 public abstract class BaseEmailTransport : IEmailTransport
 {
     public event IEmailTransport.EmailResultHandler? OnEmailResult;
+    /// <summary>
+    /// A logger to log failed attempts at sending emails.
+    /// </summary>
     protected ILogger? Logger { get; set; }
+    /// <summary>
+    /// Options that customize the behavior of email transportation.
+    /// </summary>
+    /// <seealso cref="BaseEmailTransportOptions"/>
     protected abstract BaseEmailTransportOptions Options { get; }
+    /// <summary>
+    /// Whether the email transport is currently connected or not.
+    /// </summary>
     protected abstract bool IsConnected { get; }
 
     public async Task SendQueuedEmailsAsync(IEmailQueue emailQueue, CancellationToken cancelToken = default)
@@ -22,12 +32,48 @@ public abstract class BaseEmailTransport : IEmailTransport
             }
             message = emailQueue.TryDequeue();
         }
+        await DisconnectAsync(cancelToken);
     }
-
+    /// <summary>
+    /// Connects the underlying email transport.
+    /// </summary>
+    /// <param name="cancelToken">A <see cref="CancellationToken"/> used to cancel the connection operation.</param>
+    /// <exception cref="EmailTransportException">Thrown if the connection failed.</exception>
+    /// <returns>
+    /// A <see cref="Task"/> that represents the asynchronous connection operation.
+    /// </returns>
     protected abstract Task ConnectAsync(CancellationToken cancelToken);
+    /// <summary>
+    /// Disconnects from the underlying email transport.
+    /// </summary>
+    /// <remarks>
+    /// This method should not throw an exception if the server is not connected.
+    /// </remarks>
+    /// <param name="cancelToken">A <see cref="CancellationToken"/> used to cancel the disconnect operation.</param>
+    /// <returns>
+    /// A <see cref="Task"/> that represents the asynchronous disconnect operation.
+    /// </returns>
     protected abstract Task DisconnectAsync(CancellationToken cancelToken);
+    /// <summary>
+    /// Send an email message.
+    /// </summary>
+    /// <param name="message">The <see cref="EmailMessage"/> to send.</param>
+    /// <param name="cancelToken">A <see cref="CancellationToken"/> used to cancel the sending operation.</param>
+    /// <exception cref="EmailTransportException">Thrown if the email failed to send.</exception>
+    /// <returns>
+    /// A <see cref="Task"/> that represents the asynchronous sending operation.
+    /// </returns>
     protected abstract Task SendEmailAsync(EmailMessage message, CancellationToken cancelToken);
-
+    /// <summary>
+    /// Attempts to send an <see cref="EmailMessage"/>; potentially making multiple attempts
+    /// depending on the current <see cref="Options"/>.
+    /// </summary>
+    /// <param name="message">The <see cref="EmailMessage"/> to attempt to send.</param>
+    /// <param name="cancelToken">A <see cref="CancellationToken"/> to cancel the sending operation.</param>
+    /// <returns>
+    /// A <see cref="Task"/> that represents the asynchronous sending operation.
+    /// The task result contains whether the email was successfully sent or not.
+    /// </returns>
     protected async Task<bool> TrySendEmailAsync(EmailMessage message, CancellationToken cancelToken = default)
     {
         int attempts = 0;
@@ -45,10 +91,7 @@ public abstract class BaseEmailTransport : IEmailTransport
             {
                 Logger?.LogError(ex, "Failed to send message.");
                 // Disconnect just in case if the connection is bad.
-                if (IsConnected)
-                {
-                    await DisconnectAsync(cancelToken);
-                }
+                await DisconnectAsync(cancelToken);
                 // There is no point in retrying if a fatal exception occurred.
                 if (ex.IsFatal)
                 {
